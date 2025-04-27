c6cc3498741df5ca2cc78f0f46071eb31a4650c3
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-04-27 15:24:27 +0200

    Created user mapper class and adjusted it

---
+++ auth-service/src/main/java/com/ecommerce/auth/model/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ecommerce.auth.model;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    User registerRequestToUser(RegisterRequest registerRequest);
+
+}
