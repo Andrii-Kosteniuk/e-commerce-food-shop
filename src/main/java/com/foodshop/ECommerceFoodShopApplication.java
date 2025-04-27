461b3cae8a1ac508fdcf7f4b06fcef061248f7bc
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-04-27 10:53:25 +0200

    Init commit

---
+++ src/main/java/com/foodshop/ECommerceFoodShopApplication.java
@@ -0,0 +1,13 @@
+package com.foodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceFoodShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceFoodShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodshop/ECommerceFoodShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceFoodShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
