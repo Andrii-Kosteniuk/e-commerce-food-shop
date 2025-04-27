7b564c225f7eee199efa71268c935302f9499c60
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-04-27 21:14:39 +0200

    Added 'addFilterBefore(...)' to filter chain

--- auth-service/src/main/java/com/ecommerce/auth/security/SecurityFilterChain.java
+++ auth-service/src/main/java/com/ecommerce/auth/security/SecurityFilterChain.java
@@ -1,14 +1,20 @@
 package com.ecommerce.auth.security;
 
-import lombok.RequiredArgsConstructor;
+import com.ecommerce.auth.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityFilterChain {
+
+    private final JwtAuthenticationFilter tokenFilter;
+
+    public SecurityFilterChain(JwtAuthenticationFilter tokenFilter) {
+        this.tokenFilter = tokenFilter;
+    }
 
     @Bean
     public org.springframework.security.web.SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,6 +23,7 @@ public class SecurityFilterChain {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
