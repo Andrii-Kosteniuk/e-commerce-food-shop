74d380bca923abddbfcc2f5ba71ffd5ecedbaf1f
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-04-27 21:17:04 +0200

    Created login and refresh-token endpoints.

--- auth-service/src/main/java/com/ecommerce/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/ecommerce/auth/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.ecommerce.auth.controller;
 
+import com.ecommerce.auth.jwt.JwtUtils;
+import com.ecommerce.auth.jwt.RefreshTokenRequest;
+import com.ecommerce.auth.model.AuthenticationRequest;
+import com.ecommerce.auth.model.AuthenticationResponse;
 import com.ecommerce.auth.model.RegisterRequest;
 import com.ecommerce.auth.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,20 +19,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtUtils jwtUtils;
 
     @Autowired
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, JwtUtils jwtUtils) {
         this.authService = authService;
+        this.jwtUtils = jwtUtils;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody RegisterRequest registerRequest) {
-        try{
-            authService.registerUser(registerRequest);
-            return ResponseEntity.ok("User registered successfully!");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        authService.registerUser(registerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+        authService.authenticate(request);
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthenticationResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
+        if (jwtUtils.validateToken(request.refreshToken())) {
+            throw new IllegalArgumentException("Invalid refresh token");
         }
 
+        String userName = jwtUtils.getSubjectFromToken(request.refreshToken());
+        String newAccessToken = jwtUtils.generateAccessTokenFromUserName(userName);
+
+        return ResponseEntity.ok().body(new AuthenticationResponse(newAccessToken, request.refreshToken()));
     }
 }
