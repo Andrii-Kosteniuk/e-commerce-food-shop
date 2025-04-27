8cd4233fd309c07103ec1d50805cfca763311b75
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-04-27 15:26:55 +0200

    Created basic SecurityFilterChain class

---
+++ auth-service/src/main/java/com/ecommerce/auth/security/SecurityFilterChain.java
@@ -0,0 +1,24 @@
+package com.ecommerce.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityFilterChain {
+
+    @Bean
+    public org.springframework.security.web.SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+
+    }
+
+}
