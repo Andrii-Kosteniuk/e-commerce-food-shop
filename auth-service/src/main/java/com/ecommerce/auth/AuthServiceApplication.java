b8274143289b09dd7e2a7c46d9b070f827a38bd5
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-04-29 20:09:10 +0200

    Included additional package scanning

--- auth-service/src/main/java/com/ecommerce/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ecommerce/auth/AuthServiceApplication.java
@@ -3,7 +3,7 @@ package com.ecommerce.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ecommerce.auth", "com.commonexception.exception"})
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
