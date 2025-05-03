9b759dd180289400bb7b2de2f8d7be8b62231a2a
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-03 18:02:05 +0200

    Imports were changed

--- auth-service/src/main/java/com/ecommerce/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/ecommerce/auth/controller/AuthController.java
@@ -1,15 +1,16 @@
 package com.ecommerce.auth.controller;
 
 import com.ecommerce.auth.jwt.JwtUtils;
-import com.ecommerce.auth.jwt.RefreshTokenRequest;
-import com.ecommerce.auth.model.AuthenticationRequest;
-import com.ecommerce.auth.model.AuthenticationResponse;
-import com.ecommerce.auth.model.RegisterRequest;
-import com.ecommerce.auth.service.AuthService;
+import com.ecommerce.auth.dto.RefreshTokenRequest;
+import com.ecommerce.auth.dto.AuthenticationRequest;
+import com.ecommerce.auth.dto.AuthenticationResponse;
+import com.ecommerce.auth.dto.RegisterRequest;
+import com.ecommerce.auth.service.impl.AuthServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,35 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthServiceImpl authServiceImpl;
     private final JwtUtils jwtUtils;
 
     @Autowired
-    public AuthController(AuthService authService, JwtUtils jwtUtils) {
-        this.authService = authService;
+    public AuthController(AuthServiceImpl authServiceImpl, JwtUtils jwtUtils) {
+        this.authServiceImpl = authServiceImpl;
         this.jwtUtils = jwtUtils;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody @Valid RegisterRequest registerRequest) {
-        authService.registerUser(registerRequest);
+        authServiceImpl.registerUser(registerRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully!");
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest request) {
-        authService.authenticate(request);
-        return ResponseEntity.ok(authService.authenticate(request));
+        authServiceImpl.authenticate(request);
+        return ResponseEntity.ok(authServiceImpl.authenticate(request));
     }
 
     @PostMapping("/refresh-token")
     public ResponseEntity<AuthenticationResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
         if (jwtUtils.validateToken(request.refreshToken())) {
-            throw new IllegalArgumentException("Invalid refresh token");
+            throw new BadCredentialsException("Invalid refresh token");
         }
 
         String userName = jwtUtils.getSubjectFromToken(request.refreshToken());
-        String newAccessToken = jwtUtils.generateAccessTokenFromUserName(userName);
+        String newAccessToken = jwtUtils.generateAccessTokenFromUser(authServiceImpl.getUserFromRequest(userName));
 
         return ResponseEntity.ok().body(new AuthenticationResponse(newAccessToken, request.refreshToken()));
     }
