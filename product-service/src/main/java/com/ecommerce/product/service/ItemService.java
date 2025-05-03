3e187be61d758e64472e16043828969da7bbcf4d
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-03 18:15:22 +0200

    Refactored service methods

--- product-service/src/main/java/com/ecommerce/product/service/ItemService.java
+++ product-service/src/main/java/com/ecommerce/product/service/ItemService.java
@@ -1,16 +1,17 @@
 package com.ecommerce.product.service;
 
 import com.ecommerce.product.model.Item;
-import com.ecommerce.product.model.ItemRequest;
+import com.ecommerce.product.dto.ItemCreateRequest;
+import com.ecommerce.product.dto.ItemUpdateRequest;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ItemService {
-        Item createItem(ItemRequest itemRequest);
-        Item updateItem(Long id, ItemRequest itemRequest);
+        Item createItem(ItemCreateRequest itemCreateRequest);
+        Item updateItem(Long id, ItemUpdateRequest itemUpdateRequest);
         void deleteItem(Long id);
-        Item getItemById(Long id);
+        Optional<Item> getItemById(Long id);
         List<Item> getAllItems();
         Optional<Item> findItemByName(String name);
         List<Item> findItemsByCategory(Item.Category category);
