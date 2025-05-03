3bffcb71610a5698ecbf6cc25a2f9b97ef503fda
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-03 18:00:14 +0200

    CHanged service class on interface

--- auth-service/src/main/java/com/ecommerce/auth/service/AuthService.java
+++ auth-service/src/main/java/com/ecommerce/auth/service/AuthService.java
@@ -1,60 +1,12 @@
 package com.ecommerce.auth.service;
 
-import com.ecommerce.auth.jwt.JwtUtils;
-import com.ecommerce.auth.model.*;
-import com.ecommerce.auth.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import com.ecommerce.auth.dto.AuthenticationRequest;
+import com.ecommerce.auth.dto.AuthenticationResponse;
+import com.ecommerce.auth.dto.RegisterRequest;
+import com.ecommerce.auth.model.User;
 
-@Service
-public class AuthService {
-
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtils jwtUtils;
-    private final AuthenticationManager authenticationManager;
-
-    @Autowired
-    public AuthService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtUtils = jwtUtils;
-        this.authenticationManager = authenticationManager;
-    }
-
-    public void registerUser(RegisterRequest registerRequest) {
-        if (userRepository.existsByEmail(registerRequest.email())) {
-            throw new IllegalArgumentException("User with such an email already exists");
-        }
-
-        User user = userMapper.registerRequestToUser(registerRequest);
-        user.setPassword(passwordEncoder.encode(registerRequest.password()));
-        userRepository.save(user);
-    }
-
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.email(),
-                        request.password()
-                )
-        );
-        var user = userRepository.findByEmail(request.email())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password"));
-
-        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
-            throw new BadCredentialsException("Invalid credentials");
-        }
-
-        var accessToken = jwtUtils.generateAccessTokenFromUserName(request.email());
-        var refreshToken = jwtUtils.generateRefreshToken(request.email());
-
-        return new AuthenticationResponse(accessToken, refreshToken);
-    }
+public interface AuthService {
+    void registerUser(RegisterRequest registerRequest);
+    AuthenticationResponse authenticate(AuthenticationRequest request);
+    User getUserFromRequest(String username);
 }
