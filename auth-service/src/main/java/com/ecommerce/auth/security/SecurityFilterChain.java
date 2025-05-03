5592715b0087f238771e9012f23e300f546d24e3
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-03 18:06:07 +0200

    Add filter for product service controller methods

--- auth-service/src/main/java/com/ecommerce/auth/security/SecurityFilterChain.java
+++ auth-service/src/main/java/com/ecommerce/auth/security/SecurityFilterChain.java
@@ -1,8 +1,10 @@
 package com.ecommerce.auth.security;
 
 import com.ecommerce.auth.jwt.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +24,9 @@ public class SecurityFilterChain {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/localhost:8081/api/v1/items/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/localhost:8081/api/v1/items/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/localhost:8081/api/v1/items/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
