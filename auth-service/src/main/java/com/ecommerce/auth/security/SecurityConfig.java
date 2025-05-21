13ddcfa8d97c495586aed69ee34dc2bda0e5a366
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:31:40 +0200

    Used a feign client

--- auth-service/src/main/java/com/ecommerce/auth/security/SecurityConfig.java
+++ auth-service/src/main/java/com/ecommerce/auth/security/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.ecommerce.auth.security;
 
-import com.ecommerce.auth.repository.UserRepository;
+import com.ecommerce.auth.service.UserServiceClient;
+import com.ecommerce.user.util.UserMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,21 +11,16 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserRepository userRepository;
-
-    @Autowired
-    public SecurityConfig(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserServiceClient client;
+    private final UserMapper userMapper;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -34,8 +29,7 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> (UserDetails) userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return username -> (UserDetails) userMapper.userResponceToUser(client.getUserForLogin(username));
     }
 
     @Bean
