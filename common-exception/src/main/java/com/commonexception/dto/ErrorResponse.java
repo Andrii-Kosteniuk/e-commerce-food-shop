f514d8b61919ce6531888f01081f48d045dce9a6
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:14:04 +0200

    Added JsonFormat properties

--- common-exception/src/main/java/com/commonexception/dto/ErrorResponse.java
+++ common-exception/src/main/java/com/commonexception/dto/ErrorResponse.java
@@ -1,12 +1,24 @@
 package com.commonexception.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
         LocalDateTime timestamp,
+
+        @JsonProperty(value = "status")
         int status,
+
+        @JsonProperty(value = "reason")
         String error,
+
+        @JsonProperty(value = "details")
         String message,
+
+        @JsonProperty(value = "path")
         String path
 ) {
 }
