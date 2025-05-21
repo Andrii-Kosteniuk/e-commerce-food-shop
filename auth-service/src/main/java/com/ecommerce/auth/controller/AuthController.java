b9e74ebc6014c7fdd8fc4361ab1927d964d268fd
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:10:59 +0200

    Refactored some method logic

--- auth-service/src/main/java/com/ecommerce/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/ecommerce/auth/controller/AuthController.java
@@ -1,16 +1,17 @@
 package com.ecommerce.auth.controller;
 
 import com.ecommerce.auth.jwt.JwtUtils;
-import com.ecommerce.auth.dto.RefreshTokenRequest;
-import com.ecommerce.auth.dto.AuthenticationRequest;
-import com.ecommerce.auth.dto.AuthenticationResponse;
-import com.ecommerce.auth.dto.RegisterRequest;
 import com.ecommerce.auth.service.impl.AuthServiceImpl;
+import com.commondto.auth.AuthenticationRequest;
+import com.commondto.auth.AuthenticationResponse;
+import com.commondto.auth.RefreshTokenRequest;
+import com.commondto.auth.RegisterRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +38,6 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest request) {
-        authServiceImpl.authenticate(request);
         return ResponseEntity.ok(authServiceImpl.authenticate(request));
     }
 
@@ -48,7 +48,8 @@ public class AuthController {
         }
 
         String userName = jwtUtils.getSubjectFromToken(request.refreshToken());
-        String newAccessToken = jwtUtils.generateAccessTokenFromUser(authServiceImpl.getUserFromRequest(userName));
+        UserDetails user = authServiceImpl.getUserFromRequest(userName);
+        String newAccessToken = jwtUtils.generateAccessTokenFromUser(user);
 
         return ResponseEntity.ok().body(new AuthenticationResponse(newAccessToken, request.refreshToken()));
     }
