f8f254b79dc2fb13325b0f17211bb64fa490a584
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:30:27 +0200

    Enable feign client

--- auth-service/src/main/java/com/ecommerce/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ecommerce/auth/AuthServiceApplication.java
@@ -1,9 +1,12 @@
 package com.ecommerce.auth;
 
+import com.ecommerce.auth.service.UserServiceClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.ecommerce.auth", "com.commonexception.exception"})
+@EnableFeignClients(clients = UserServiceClient.class)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
