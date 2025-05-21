6f0b72793193b89c31b5633fe2c10fd3e2f2f42d
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:12:07 +0200

    Ignore commit

--- auth-service/src/main/java/com/ecommerce/auth/dto/AuthenticationResponse.java
+++
@@ -1,6 +0,0 @@
-package com.ecommerce.auth.dto;
-
-public record AuthenticationResponse(
-        String token,
-        String refreshToken
-) {}
--- auth-service/src/main/java/com/ecommerce/auth/dto/RefreshTokenRequest.java
+++
@@ -1,3 +0,0 @@
-package com.ecommerce.auth.dto;
-
-public record RefreshTokenRequest(String refreshToken) {}
