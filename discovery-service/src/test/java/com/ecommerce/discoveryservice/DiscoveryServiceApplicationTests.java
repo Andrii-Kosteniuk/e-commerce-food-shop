1408fd3dc02f6774432b07c9731a532c606e812e
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:03:27 +0200

    Init

--- discovery-service/src/test/java/com/ecommerce/discoveryservice/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/com/ecommerce/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ecommerce.product;
+package com.ecommerce.discoveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductServiceApplicationTests {
+class DiscoveryServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
