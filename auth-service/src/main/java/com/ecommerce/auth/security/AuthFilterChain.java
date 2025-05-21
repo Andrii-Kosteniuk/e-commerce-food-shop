05bfe0c2ce123eace3b6b941232dd05c80565e62
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-21 13:05:35 +0200

    Created a basic filter chain

---
+++ auth-service/src/main/java/com/ecommerce/auth/security/AuthFilterChain.java
@@ -0,0 +1,30 @@
+package com.ecommerce.auth.security;
+
+import com.ecommerce.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthFilterChain {
+
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChainAuth(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+}
