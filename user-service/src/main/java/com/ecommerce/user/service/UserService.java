8ab2901aba32d3e5e98084fc6dd11ce29102f9ac
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-23 14:45:08 +0200

    Removed getUserAddress method implementation

--- user-service/src/main/java/com/ecommerce/user/service/UserService.java
+++ user-service/src/main/java/com/ecommerce/user/service/UserService.java
@@ -1,7 +1,6 @@
 package com.ecommerce.user.service;
 
 import com.commondto.auth.RegisterRequest;
-import com.ecommerce.user.model.Address;
 import com.ecommerce.user.model.User;
 
 public interface UserService {
@@ -9,7 +8,5 @@ public interface UserService {
     User createUser(RegisterRequest request);
     User getUserById(Long id);
     User getUserByEmail(String email);
-    Address getUserAddress(Long userId);
-
     User getRegisteredUser(String email);
 }
