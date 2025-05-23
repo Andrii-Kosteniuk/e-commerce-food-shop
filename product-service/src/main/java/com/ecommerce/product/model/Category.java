d79686ffffe641f1d3214f1f3635922b170c5476
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-23 12:56:54 +0200

    The category was separated from the model class

---
+++ product-service/src/main/java/com/ecommerce/product/model/Category.java
@@ -0,0 +1,18 @@
+package com.ecommerce.product.model;
+
+public enum Category {
+
+        BEVERAGES,
+        GROCERIES,
+        PASTRIES,
+        VEGETABLES,
+        FRUITS,
+        DAIRY,
+        MEAT,
+        SEAFOOD,
+        SNACKS,
+        BAKERY,
+        FROZEN_FOODS,
+        ORGANIC
+
+}
