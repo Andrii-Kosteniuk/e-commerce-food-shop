229d97b603f230e8a1c83f902edec40d104517de
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-26 11:09:55 +0200

    Changed user response class

--- common-dto/src/main/java/com/commondto/user/UserResponse.java
+++ common-dto/src/main/java/com/commondto/user/UserResponse.java
@@ -1,14 +1,13 @@
 package com.commondto.user;
 
 
-import com.ecommerce.deliveryservice.model.DeliveryInfo;
-
 public record UserResponse (
 
         String firstName,
         String lastName,
         String email,
-        DeliveryInfo deliveryInfo
+        String password,
+        String role
 ) {
 
 }
