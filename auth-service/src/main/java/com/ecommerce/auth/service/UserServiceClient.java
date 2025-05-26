3dccc876306dea83d5fd3dbd7c9adf194bded3b6
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-26 11:11:00 +0200

    Changed method in user service client class

--- auth-service/src/main/java/com/ecommerce/auth/service/UserServiceClient.java
+++ auth-service/src/main/java/com/ecommerce/auth/service/UserServiceClient.java
@@ -1,13 +1,10 @@
 package com.ecommerce.auth.service;
 
-import com.ecommerce.auth.config.FeignClientConfig;
+import com.ecommerce.auth.feign.FeignClientConfig;
 import com.commondto.auth.RegisterRequest;
 import com.commondto.user.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "user-service", path = "/api/v1/users", configuration = FeignClientConfig.class)
 public interface UserServiceClient {
@@ -15,7 +12,7 @@ public interface UserServiceClient {
     @PostMapping("/register-user")
     UserResponse createNewUser(@RequestBody RegisterRequest request);
 
-    @GetMapping("/login-user")
-    UserResponse getUserForLogin(@RequestParam("email") String email);
+    @GetMapping("/{email}")
+    UserResponse getUserForLogin(@PathVariable("email") String email);
 
 }
