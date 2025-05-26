bfb59b986fb65f9479751b611bdf48eab7ca160e
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-26 11:15:41 +0200

    Created AuthMapper class

---
+++ auth-service/src/main/java/com/ecommerce/auth/security/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package com.ecommerce.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final String email;
+    private final String password;
+    private final String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
