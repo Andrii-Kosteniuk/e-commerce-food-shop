8df795ba16afcbf3f1d3cedefb38546af90f3ec9
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-26 11:08:35 +0200

    Exclude UserDetails implementation from entity class

--- user-service/src/main/java/com/ecommerce/user/model/User.java
+++ user-service/src/main/java/com/ecommerce/user/model/User.java
@@ -4,19 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -36,15 +30,4 @@ public class User implements UserDetails {
 
     @Column
     private String role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
 }
