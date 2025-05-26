8f9d570873609b25032171577c221a008913f1a4
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-26 11:09:32 +0200

    Refactored methods

--- user-service/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user-service/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -4,9 +4,9 @@ import com.commondto.auth.RegisterRequest;
 import com.commondto.user.UserResponse;
 import com.ecommerce.user.model.User;
 import com.ecommerce.user.service.UserService;
-import com.ecommerce.user.util.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,18 +16,16 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-    private final UserMapper userMapper;
 
     @PostMapping("/register-user")
-    public UserResponse createNewUser(@RequestBody RegisterRequest request) {
-        return userMapper.userToUserResponse(userService.createUser(request));
+    public User createNewUser(@RequestBody RegisterRequest request) {
+        return userService.createUser(request);
     }
 
 
-    @GetMapping("/login-user")
-    public UserResponse getUserForLogin(String email) {
-        User user = userService.getRegisteredUser(email);
-        return userMapper.userToUserResponse(user);
+    @GetMapping("/{email}")
+    public ResponseEntity<UserResponse> getUserForLogin(@PathVariable String email) {
+        return ResponseEntity.ok(userService.getRegisteredUser(email));
     }
 
     @GetMapping("/authenticated-user")
