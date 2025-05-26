8b72a65bd00656e58278ff2355e2bb2d68aa3d25
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-05-26 11:10:27 +0200

    Changed method in user service class

--- user-service/src/main/java/com/ecommerce/user/service/UserService.java
+++ user-service/src/main/java/com/ecommerce/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.user.service;
 
 import com.commondto.auth.RegisterRequest;
+import com.commondto.user.UserResponse;
 import com.ecommerce.user.model.User;
 
 public interface UserService {
@@ -8,6 +9,6 @@ public interface UserService {
     User createUser(RegisterRequest request);
     User getUserById(Long id);
     User getUserByEmail(String email);
-    User getRegisteredUser(String email);
+    UserResponse getRegisteredUser(String email);
     User getAuthenticatedUser();
 }
