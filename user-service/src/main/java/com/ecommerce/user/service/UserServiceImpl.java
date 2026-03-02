d65826337625cc0729ad929679c9a9dabaafe598
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-02 09:58:33 +0100

    Use mapper

--- user-service/src/main/java/com/ecommerce/user/service/UserServiceImpl.java
+++ user-service/src/main/java/com/ecommerce/user/service/UserServiceImpl.java
@@ -10,7 +10,6 @@ import com.ecommerce.user.model.UserMapper;
 import com.ecommerce.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -20,7 +19,6 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
 
     @Override
@@ -32,12 +30,7 @@ public class UserServiceImpl implements UserService {
             throw new ResourceAlreadyExistsException("User with such an email already exists");
         }
 
-        User user = new User();
-        user.setFirstName(request.firstName());
-        user.setLastName(request.lastName());
-        user.setEmail(request.email());
-        user.setPassword(passwordEncoder.encode(request.password()));
-        user.setRole(request.role());
+        User user = userMapper.requestToUser(request);
 
         log.info("Creating new user in user-service:");
         return userRepository.save(user);
