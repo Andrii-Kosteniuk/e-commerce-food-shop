9d57760350c5f3818af7f7e579f7a9335df779be
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-02 09:59:30 +0100

    Delete authentication provider

--- auth-service/src/main/java/com/ecommerce/auth/security/SecurityConfig.java
+++ auth-service/src/main/java/com/ecommerce/auth/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -42,14 +41,6 @@ public class SecurityConfig {
 
             return authMapper.toUserDetails(user);
         };
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setPasswordEncoder(passwordEncoder());
-        authProvider.setUserDetailsService(userDetailsService());
-        return authProvider;
     }
 
     @Bean
