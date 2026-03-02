417fe6eddec761420323bf6df67806b01601012e
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-02 10:01:30 +0100

    FIx mapper

--- auth-service/src/main/java/com/ecommerce/auth/mapper/AuthMapper.java
+++ auth-service/src/main/java/com/ecommerce/auth/mapper/AuthMapper.java
@@ -3,8 +3,9 @@ package com.ecommerce.auth.mapper;
 import com.ecommerce.commondto.user.UserResponse;
 import com.ecommerce.auth.security.CustomUserDetails;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingInheritanceStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", mappingInheritanceStrategy = MappingInheritanceStrategy.EXPLICIT)
 public interface AuthMapper {
 
     CustomUserDetails toUserDetails(UserResponse userResponse);
