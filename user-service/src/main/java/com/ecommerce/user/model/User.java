4b766a27e6ca53ffb662f5481babf69cdb767291
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-15 12:51:24 +0100

    Refactored user-service

--- user-service/src/main/java/com/ecommerce/user/model/User.java
+++ user-service/src/main/java/com/ecommerce/user/model/User.java
@@ -29,6 +29,6 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
