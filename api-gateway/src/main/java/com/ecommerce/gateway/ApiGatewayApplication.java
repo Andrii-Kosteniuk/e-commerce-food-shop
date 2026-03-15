720eacf043a18e2987fd2aa4a3f8aea69c23f108
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-15 12:54:14 +0100

    Refactored apigateway-service

--- api-gateway/src/main/java/com/ecommerce/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecommerce/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.user.apigateway;
+package com.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
