944a0d3f752d83b6ee6003cf762634738a169d92
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-18 13:31:09 +0100

    Create OrderItemResponse class

---
+++ common-dto/src/main/java/com/ecommerce/commondto/order/OrderItemResponse.java
@@ -0,0 +1,9 @@
+package com.ecommerce.commondto.order;
+
+import java.math.BigDecimal;
+
+public record OrderItemResponse(
+        Long productId,
+        String productName,
+        BigDecimal price
+) {}
