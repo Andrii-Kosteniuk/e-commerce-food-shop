04d3c28f19ded51525799c4d02ae8d74645a3243
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-03-28 14:21:59 +0100

    Add method to get user by email

--- order-service/src/main/java/com/ecommerce/order/feign/UserFeignClient.java
+++ order-service/src/main/java/com/ecommerce/order/feign/UserFeignClient.java
@@ -3,9 +3,11 @@ package com.ecommerce.order.feign;
 
 import com.ecommerce.commondto.user.UserResponse;
 import com.study.feignclientconfig.feign.FeignClientConfig;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service", path = "/api/v1/internal/users", configuration = FeignClientConfig .class)
 public interface UserFeignClient {
@@ -14,4 +16,6 @@ public interface UserFeignClient {
     UserResponse getUserById(@PathVariable Long id);
 
 
+    @GetMapping("/email")
+    UserResponse getUserByEmail(@RequestParam String email);
 }
