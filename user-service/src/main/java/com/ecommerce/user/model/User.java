670c0bd471a7345d8386eee24fd37fa9d8b2a36d
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-06 19:27:09 +0200

    Refactored user domain

--- user-service/src/main/java/com/ecommerce/user/model/User.java
+++ user-service/src/main/java/com/ecommerce/user/model/User.java
@@ -4,16 +4,23 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq_generator", allocationSize = 1)
     @Column(name = "user_id")
     private Long id;
 
@@ -31,4 +38,14 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return password;
+    }
 }
