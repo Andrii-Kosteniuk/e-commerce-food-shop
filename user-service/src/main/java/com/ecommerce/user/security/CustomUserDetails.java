b1fdf0853cacb96e7f474ba3168296e3a28d6f4d
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-06 19:32:09 +0200

    Moved to user-service

---
+++ user-service/src/main/java/com/ecommerce/user/security/CustomUserDetails.java
@@ -0,0 +1,24 @@
+package com.ecommerce.user.security;
+
+import com.ecommerce.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CustomUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Loading user by username: {}", username);
+        return   userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
