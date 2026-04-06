2f4648a128e387173a821fbddfffe5ee4f0663b8
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-06 19:40:47 +0200

    Create email service

---
+++ notification-service/src/main/java/com/ecommerce/notification/mail/EmailService.java
@@ -0,0 +1,36 @@
+package com.ecommerce.notification.mail;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService  {
+
+    @Value("${notification.from-email}")
+    private String fromEmail;
+
+    private final MailSender mailSender;
+
+    public void sendEmail(String toEmail, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(toEmail);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        } catch (Exception e) {
+            log.error("Failed to send email to: {}, error: {}", toEmail, e.getMessage());
+        }
+
+    }
+
+
+}
