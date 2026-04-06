d145871d5d6ac2c6d45f797dea937283fdba8fd1
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-06 19:22:23 +0200

    Add scan by packages

--- user-service/src/main/java/com/ecommerce/user/UserServiceApplication.java
+++ user-service/src/main/java/com/ecommerce/user/UserServiceApplication.java
@@ -3,11 +3,10 @@ package com.ecommerce.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ecommerce.user", "com.ecommerce.commonexception", "com.ecommerce.commondto", "com.ecommerce.security"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
