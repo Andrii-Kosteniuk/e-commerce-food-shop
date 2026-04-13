b4057ab4cab9a61d223674ed740b31b5806ef74c
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-13 10:30:42 +0200

    Add order.confirmed topic

--- kafka-service/src/main/java/com/ecommerce/kafka/config/KafkaTopics.java
+++ kafka-service/src/main/java/com/ecommerce/kafka/config/KafkaTopics.java
@@ -3,6 +3,7 @@ package com.ecommerce.kafka.config;
 public class KafkaTopics {
 
     public static final String ORDER_CREATED        = "order.created";
+    public static final String ORDER_CONFIRMED      = "order.confirmed";
     public static final String PAYMENT_SUCCEEDED    = "payment.succeeded";
     public static final String PAYMENT_FAILED       = "payment.failed";
     public static final String STOCK_RESERVED       = "stock.reserved";
