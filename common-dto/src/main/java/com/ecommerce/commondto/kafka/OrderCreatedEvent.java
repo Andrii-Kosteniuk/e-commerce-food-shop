5988bcacf5074b42918d1a9765b4dd3d74106ba1
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-13 10:28:25 +0200

    Expand OrderCreatedEvent DTO

--- common-dto/src/main/java/com/ecommerce/commondto/kafka/OrderCreatedEvent.java
+++ common-dto/src/main/java/com/ecommerce/commondto/kafka/OrderCreatedEvent.java
@@ -1,5 +1,7 @@
 package com.ecommerce.commondto.kafka;
 
+import com.ecommerce.commondto.order.OrderResponse;
+
 import java.math.BigDecimal;
 
-public record OrderCreatedEvent(Long orderId, Long userId, String userEmail, BigDecimal totalPrice, String status) {}
+public record OrderCreatedEvent(Long orderId, Long userId, String userEmail, BigDecimal totalPrice, String status, OrderResponse response) {}
