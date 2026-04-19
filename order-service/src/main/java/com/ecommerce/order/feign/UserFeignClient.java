ddc947cc66565aa8720305f24d3caca3b77aeb2d
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-19 14:49:52 +0200

    Add fallbackFactory component

--- order-service/src/main/java/com/ecommerce/order/feign/UserFeignClient.java
+++ order-service/src/main/java/com/ecommerce/order/feign/UserFeignClient.java
@@ -3,16 +3,22 @@ package com.ecommerce.order.feign;
 
 import com.ecommerce.commondto.user.UserResponse;
 import com.ecommerce.feignconfig.FeignClientConfig;
+import com.ecommerce.order.feign.fallback.UserFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", path = "/api/v1/internal/users", configuration = FeignClientConfig .class)
+import javax.naming.ServiceUnavailableException;
+
+@FeignClient(
+        name = "user-service",
+        path = "/api/v1/internal/users",
+        fallbackFactory =  UserFeignClientFallbackFactory.class)
 public interface UserFeignClient {
 
     @GetMapping("/{id}")
-    UserResponse getUserById(@PathVariable Long id);
+    UserResponse getUserById(@PathVariable Long id) throws ServiceUnavailableException;
 
 
     @GetMapping("/email")
