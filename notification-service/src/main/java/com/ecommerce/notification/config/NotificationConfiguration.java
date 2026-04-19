1b85e5195b8e0c4a6865088b1847e101dac329c9
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-19 17:00:39 +0200

    Remove unnecessary NotificationConfiguration

---
+++ notification-service/src/main/java/com/ecommerce/notification/config/NotificationConfiguration.java
@@ -0,0 +1,16 @@
+//package com.ecommerce.notification.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.MailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//
+//@Configuration
+//public class NotificationConfiguration {
+//
+//    @Bean
+//    public MailSender mailSenderAgent() {
+//       return new JavaMailSenderImpl();
+//    }
+//
+//}
