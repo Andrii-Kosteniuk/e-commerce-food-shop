bf62808435e9caa343f04bd32cc8932b673b64e1
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-19 17:28:41 +0200

    Refactoring

--- order-service/src/main/java/com/ecommerce/order/feign/UserFeignClient.java
+++ order-service/src/main/java/com/ecommerce/order/feign/UserFeignClient.java
@@ -2,14 +2,11 @@ package com.ecommerce.order.feign;
 
 
 import com.ecommerce.commondto.user.UserResponse;
-import com.ecommerce.feignconfig.FeignClientConfig;
 import com.ecommerce.order.feign.fallback.UserFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.naming.ServiceUnavailableException;
 
 @FeignClient(
         name = "user-service",
