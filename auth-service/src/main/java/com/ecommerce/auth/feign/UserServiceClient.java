cd251c2479a665f330b1c3015efd395f5e18fe66
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-19 14:47:35 +0200

    Add fallbackFactory component

--- auth-service/src/main/java/com/ecommerce/auth/feign/UserServiceClient.java
+++ auth-service/src/main/java/com/ecommerce/auth/feign/UserServiceClient.java
@@ -1,11 +1,16 @@
 package com.ecommerce.auth.feign;
 
+import com.ecommerce.auth.feign.fallback.UserServiceClientFallbackFactory;
 import com.ecommerce.commondto.auth.*;
 import com.ecommerce.feignconfig.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "user-service", path = "/api/v1/internal/users", configuration = FeignClientConfig.class)
+@FeignClient(
+        name = "user-service",
+        path = "/api/v1/internal/users",
+        configuration = FeignClientConfig.class,
+        fallbackFactory = UserServiceClientFallbackFactory.class)
 public interface UserServiceClient {
 
     @PostMapping("/register")
