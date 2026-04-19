35204a66a9c976794c4de5f894b6493e3c611ca5
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-19 14:50:49 +0200

    Include basePackages and defaultConfiguration

--- order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
@@ -1,11 +1,14 @@
 package com.ecommerce.order;
 
+import com.ecommerce.feignconfig.FeignClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.ecommerce.order","com.ecommerce.commonexception", "com.ecommerce.commondto", "com.ecommerce.kafka", "com.ecommerce.security"})
-@EnableFeignClients
+@EnableFeignClients(
+        basePackages = "com.ecommerce.order.feign",
+        defaultConfiguration = FeignClientConfig.class)
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
