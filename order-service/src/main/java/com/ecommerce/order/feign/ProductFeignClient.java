de5616c249a1f1466938aabb84bcca57adc8086b
Andrii Kosteniuk <akosteniuk86@gmail.com>
2026-04-19 14:49:43 +0200

    Add fallbackFactory component

--- order-service/src/main/java/com/ecommerce/order/feign/ProductFeignClient.java
+++ order-service/src/main/java/com/ecommerce/order/feign/ProductFeignClient.java
@@ -2,11 +2,15 @@ package com.ecommerce.order.feign;
 
 import com.ecommerce.commondto.product.ProductResponse;
 import com.ecommerce.feignconfig.FeignClientConfig;
+import com.ecommerce.order.feign.fallback.ProductFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", path = "/api/v1/products", configuration = FeignClientConfig.class )
+@FeignClient(
+        name = "product-service",
+        path = "/api/v1/products",
+        fallbackFactory = ProductFeignClientFallbackFactory.class)
 public interface ProductFeignClient {
 
     @GetMapping("/{id}")
