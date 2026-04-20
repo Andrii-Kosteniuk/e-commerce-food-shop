81ee02b2e5ee44c6c5d78b78dc540e7fa3a3a294
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-20 12:16:40 +0200

    Inject internalApiKey in FeignAuthInterceptor

--- feign-client-config/src/main/java/com/ecommerce/feignconfig/FeignClientConfig.java
+++ feign-client-config/src/main/java/com/ecommerce/feignconfig/FeignClientConfig.java
@@ -5,11 +5,15 @@ import com.ecommerce.feignconfig.decoder.CustomFeignErrorDecoder;
 import com.ecommerce.feignconfig.interceptor.FeignAuthInterceptor;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignClientConfig {
+
+    @Value("${security.internal-api-key}")
+    private String internalApiKey;
 
     @Bean
     public ErrorDecoder errorDecoder() {
@@ -18,7 +22,7 @@ public class FeignClientConfig {
 
     @Bean
     public FeignAuthInterceptor feignAuthInterceptor() {
-        return new FeignAuthInterceptor();
+        return new FeignAuthInterceptor(internalApiKey);
     }
 
     @Bean
