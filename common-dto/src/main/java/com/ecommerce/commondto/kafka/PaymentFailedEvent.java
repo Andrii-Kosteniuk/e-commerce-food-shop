7876d1f71e0a48ba9bef8fe52913d0e19b597bab
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-21 18:33:55 +0200

    Create event class

--- common-dto/src/main/java/com/ecommerce/commondto/kafka/PaymentFailedEvent.java
+++ common-dto/src/main/java/com/ecommerce/commondto/kafka/PaymentFailedEvent.java
@@ -1,8 +1,8 @@
 package com.ecommerce.commondto.kafka;
 
 public record PaymentFailedEvent(
+        Long paymentId,
         Long orderId,
         Long userId,
-        String userEmail,
         String reason
 ) {}
--- common-dto/src/main/java/com/ecommerce/commondto/kafka/PaymentSucceededEvent.java
+++ common-dto/src/main/java/com/ecommerce/commondto/kafka/PaymentSucceededEvent.java
@@ -1,15 +1,8 @@
 package com.ecommerce.commondto.kafka;
 
-import com.ecommerce.commondto.order.StockItem;
-
-import java.math.BigDecimal;
-import java.util.List;
-
 public record PaymentSucceededEvent(
+        Long paymentId,
         Long orderId,
         Long userId,
-        String userEmail,
-        BigDecimal totalPrice,
-        String transactionId,
-        List<StockItem> items
+        String stripePaymentId
 ) {}
