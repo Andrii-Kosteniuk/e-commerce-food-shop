5dcc4fa4f4fe74141fbbd0ebf2bc40210e003a1c
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-22 14:41:11 +0200

    Fix payment feign client

--- order-service/src/main/java/com/ecommerce/order/feign/PaymentFeignClient.java
+++ order-service/src/main/java/com/ecommerce/order/feign/PaymentFeignClient.java
@@ -2,16 +2,22 @@ package com.ecommerce.order.feign;
 
 import com.ecommerce.commondto.payment.PaymentRequest;
 import com.ecommerce.commondto.payment.PaymentResponse;
+import com.ecommerce.order.feign.fallback.PaymentFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "payment-service",
-        path = "/api/v1/payments")
+        path = "/api/v1/payments",
+        fallbackFactory = PaymentFeignClientFallbackFactory.class)
 public interface PaymentFeignClient {
 
-    @PostMapping("/create")
-    ResponseEntity<PaymentResponse> createPayment(PaymentRequest request);
+    @PostMapping
+    PaymentResponse createPayment(@RequestBody PaymentRequest request);
 
+    @GetMapping("/order/{orderId}")
+    PaymentResponse getPaymentByOrderId(@PathVariable Long orderId);
 }
