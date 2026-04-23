38734a08440cb9d1cefe08e90a6127bcdb85e447
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-23 17:57:47 +0200

    Modify OrderEventConsumer

--- order-service/src/main/java/com/ecommerce/order/kafka/OrderEventConsumer.java
+++ order-service/src/main/java/com/ecommerce/order/kafka/OrderEventConsumer.java
@@ -1,12 +1,8 @@
 package com.ecommerce.order.kafka;
 
-import com.ecommerce.commondto.kafka.OrderConfirmedEvent;
-import com.ecommerce.commonexception.exception.KafkaEventException;
 import com.ecommerce.kafka.topic.KafkaTopics;
-import com.ecommerce.commondto.kafka.OrderCanceledEvent;
 import com.ecommerce.commondto.kafka.PaymentFailedEvent;
 import com.ecommerce.commondto.kafka.PaymentSucceededEvent;
-import com.ecommerce.order.feign.PaymentFeignClient;
 import com.ecommerce.order.model.OrderStatus;
 import com.ecommerce.order.repository.OrderRepository;
 import com.ecommerce.order.service.OrderModifiedService;
@@ -22,8 +18,6 @@ public class OrderEventConsumer {
 
     private final OrderModifiedService orderModifiedService;
     private final OrderRepository orderRepository;
-    private final PaymentFeignClient paymentFeignClient;
-
 
     @KafkaListener(
             topics = KafkaTopics.PAYMENT_SUCCEEDED,
@@ -42,43 +36,13 @@ public class OrderEventConsumer {
     }
 
 
-    @KafkaListener(
+        @KafkaListener(
             topics = KafkaTopics.PAYMENT_FAILED,
             groupId = "order-group"
     )
     public void handlePaymentFailed(PaymentFailedEvent event) {
-        log.info("Payment failed for orderId: {}, reason: {}",
-                event.orderId(), event.reason());
+        log.info("Payment failed for orderId: {}, reason: {}", event.orderId(), event.reason());
         orderModifiedService.cancelOrder(event.orderId());
-        orderModifiedService.updateOrderStatus(event.orderId(), OrderStatus.CANCELLED);
-
     }
 
-    @KafkaListener(
-            topics = KafkaTopics.ORDER_CONFIRMED,
-            groupId = "order-group"
-    )
-    public void handleOrderConfirmed(OrderConfirmedEvent event) {
-        try {
-            orderModifiedService.updateOrderStatus(event.orderId(), OrderStatus.CONFIRMED);
-            log.info("Order {} confirmed", event.orderId());
-
-        } catch (Exception e) {
-            throw new KafkaEventException("Failed to confirm order", e);
-        }
-
-    }
-
-    @KafkaListener(
-            topics = KafkaTopics.ORDER_CANCELED,
-            groupId = "order-group"
-    )
-    public void handleOrderCanceled(OrderCanceledEvent event) {
-        try {
-            orderModifiedService.updateOrderStatus(event.orderId(), OrderStatus.CANCELLED);
-        } catch (Exception e) {
-            throw new KafkaEventException("Failed to confirm order", e);
-        }
-
-    }
 }
