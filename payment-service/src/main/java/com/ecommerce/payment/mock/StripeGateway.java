063fab3f0b859257ccb0920d05e1580f789b3c9a
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-23 17:52:52 +0200

    Create simulation of generating random stripePaymentId

--- payment-service/src/main/java/com/ecommerce/payment/mock/StripeGateway.java
+++ payment-service/src/main/java/com/ecommerce/payment/mock/StripeGateway.java
@@ -3,14 +3,17 @@ package com.ecommerce.payment.mock;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.Random;
+import java.util.UUID;
 
 @Component
 public class StripeGateway {
 
     public String processPayment(String currency, BigDecimal amount) {
 
-        return "ch_" + java.util.UUID.randomUUID().toString().substring(0, 24);
+        boolean result = new Random().nextBoolean();
+
+        return result ? "ch_" + UUID.randomUUID().toString().substring(0, 25)
+                : "";
     }
-
-
 }
