959d0dbde6c7b475db9fa66d22170ac2916c1a05
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-24 12:33:03 +0200

    Use entity for changing status not just id

--- order-service/src/main/java/com/ecommerce/order/service/OrderModifiedService.java
+++ order-service/src/main/java/com/ecommerce/order/service/OrderModifiedService.java
@@ -2,13 +2,14 @@ package com.ecommerce.order.service;
 
 import com.ecommerce.commondto.order.OrderCreateRequest;
 import com.ecommerce.commondto.order.OrderResponse;
+import com.ecommerce.order.model.Order;
 import com.ecommerce.order.model.OrderStatus;
 
 public interface OrderModifiedService {
 
     OrderResponse createOrder(String email, OrderCreateRequest request);
-    OrderResponse confirmOrder(Long orderId);
-    void updateOrderStatus(Long orderId, OrderStatus newStatus);
+    OrderResponse confirmOrder(Long orderId, Long userId);
+    void updateOrderStatus(Order order, OrderStatus newStatus);
     OrderResponse cancelOrder(Long orderId);
     void deleteOrder(Long id);
 
