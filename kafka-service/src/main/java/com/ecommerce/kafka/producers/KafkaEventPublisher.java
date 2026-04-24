e9182d7aacce9dfcfe0852c298a42ab4009d2926
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-04-24 12:31:01 +0200

    Fixed EventPublisher

--- kafka-service/src/main/java/com/ecommerce/kafka/producers/KafkaEventPublisher.java
+++ kafka-service/src/main/java/com/ecommerce/kafka/producers/KafkaEventPublisher.java
@@ -8,12 +8,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaEventPublisher {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void publish(String topic, String id,  Object event) {
-        kafkaTemplate.send(topic, id, event);
+
+        log.info("Publishing {} event for ID {}", event.getClass().getSimpleName(), id);
+
+        kafkaTemplate.send(topic, id, event)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log.error("Failed to publish {} for ID={} reason: {}" , event.getClass().getSimpleName(), id, ex.getMessage());
+                    } else {
+                        log.info("Event published successfully for ID={}, offset={}",
+                                id,  result.getRecordMetadata().offset());
+                    }
+                });
     }
 
 }
