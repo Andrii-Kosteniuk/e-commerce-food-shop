b3458f30b10dce2b5636619ff5fa35b1c0f44a2a
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-06-29 17:43:03 +0200

    Create RedisCacheConfig for users

---
+++ user-service/src/main/java/com/ecommerce/user/config/RedisCacheConfig.java
@@ -0,0 +1,39 @@
+package com.ecommerce.user.config;
+
+import com.ecommerce.commondto.user.UserResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<UserResponse> userSerializer =
+                new Jackson2JsonRedisSerializer<>(mapper, UserResponse.class);
+
+        RedisCacheConfiguration usersConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(userSerializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(usersConfig)
+                .build();
+    }
+}
