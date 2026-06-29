dbec1851a46315a93c29813207fc9ec749a5e082
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-06-29 17:42:42 +0200

    Add topic

--- kafka-service/src/main/java/com/ecommerce/kafka/utils/KafkaTopics.java
+++ kafka-service/src/main/java/com/ecommerce/kafka/utils/KafkaTopics.java
@@ -1,12 +1,15 @@
-package com.ecommerce.kafka.topic;
+package com.ecommerce.kafka.utils;
 
 public class KafkaTopics {
-
     public static final String ORDER_CREATED        = "order.created";
     public static final String ORDER_CONFIRMED      = "order.confirmed";
     public static final String ORDER_CANCELED       = "order.canceled";
+    public static final String PAYMENT_CREATE       = "payment.create";
     public static final String PAYMENT_SUCCEEDED    = "payment.succeeded";
     public static final String PAYMENT_FAILED       = "payment.failed";
     public static final String STOCK_RESERVED       = "stock.reserved";
     public static final String STOCK_RELEASED       = "stock.released";
+
+    private KafkaTopics() {
+    }
 }
