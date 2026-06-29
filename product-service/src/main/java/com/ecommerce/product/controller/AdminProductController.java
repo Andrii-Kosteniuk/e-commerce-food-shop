9b6b70fa41e5a438a53a999590ea70f99d252d16
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-06-29 17:32:08 +0200

    Remove unnecessarily restriction on method

--- product-service/src/main/java/com/ecommerce/product/controller/AdminProductController.java
+++ product-service/src/main/java/com/ecommerce/product/controller/AdminProductController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,21 +17,18 @@ public class AdminProductController {
 
     private final ProductManagementService productService;
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
         ProductResponse productResponse = productService.createProduct(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductUpdateRequest request) {
         ProductResponse productResponse = productService.updateProduct(id, request);
         return ResponseEntity.ok(productResponse);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
