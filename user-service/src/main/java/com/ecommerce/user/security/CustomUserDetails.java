cb14d4a62c523bf18a5173c3f1eba2dc501a0a1f
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-06-29 17:34:51 +0200

    Change exception

--- user-service/src/main/java/com/ecommerce/user/security/CustomUserDetails.java
+++ user-service/src/main/java/com/ecommerce/user/security/CustomUserDetails.java
@@ -3,9 +3,9 @@ package com.ecommerce.user.security;
 import com.ecommerce.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,9 +16,9 @@ public class CustomUserDetails implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         log.info("Loading user by username: {}", username);
         return   userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new BadCredentialsException("User not found"));
     }
 }
