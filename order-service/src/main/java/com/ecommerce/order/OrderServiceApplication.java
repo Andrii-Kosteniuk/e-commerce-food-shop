0376d0624ac2187a09b350f292e9578e26a32607
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-07-02 23:57:34 +0200

    Add clients = {ProductFeignClient.class, UserFeignClient.class}

--- order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
@@ -1,6 +1,8 @@
 package com.ecommerce.order;
 
 import com.ecommerce.feignconfig.FeignClientConfig;
+import com.ecommerce.order.feign.ProductFeignClient;
+import com.ecommerce.order.feign.UserFeignClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +14,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         "com.ecommerce.kafka",
         "com.ecommerce.security"})
 @EnableFeignClients(
-        defaultConfiguration = FeignClientConfig.class)
+        defaultConfiguration = FeignClientConfig.class,
+        clients = {ProductFeignClient.class, UserFeignClient.class})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
