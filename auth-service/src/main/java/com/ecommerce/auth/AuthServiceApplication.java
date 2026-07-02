61be5022e741c994d2a39e28853e9cd64814b507
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2026-07-03 00:01:25 +0200

    Add defaultConfiguration = FeignClientConfig.class

--- auth-service/src/main/java/com/ecommerce/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ecommerce/auth/AuthServiceApplication.java
@@ -1,6 +1,7 @@
 package com.ecommerce.auth;
 
 import com.ecommerce.auth.feign.UserServiceClient;
+import com.ecommerce.feignconfig.FeignClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,7 +11,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         "com.ecommerce.commondto",
         "com.ecommerce.commonexception",
         "com.ecommerce.feignconfig"})
-@EnableFeignClients(clients = UserServiceClient.class)
+@EnableFeignClients(
+        defaultConfiguration = FeignClientConfig.class,
+        clients = UserServiceClient.class)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
